31a4e8c7570346f344aaaef72951bf1349aebc14
Edgar Nicolás Aguilera <edgar.nicolas.aguilera@gmail.com>
2019-03-12 22:08:46 -0300

    Eliminando dependencias sin utilizar

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,6 @@
 
 import express from 'express';
 import cors from 'cors';
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-//const path = require('path');
 
 import Options from './options';
 import ApiRouter from './api/api.router';
@@ -20,12 +17,6 @@ const CORSMiddleware = cors(corsOptions);
 app.options('*', CORSMiddleware)
 app.use(CORSMiddleware);
 app.use(`${Options.API_URI}`, ApiRouter);
-
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
 
 // Serve the static files from the React app
 //app.use(express.static(path.join(__dirname, 'client/build')));
