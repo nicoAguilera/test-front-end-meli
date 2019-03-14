4838a387d33157cd93994c7ff7a36ce7e940b8c6
Edgar Nicolás Aguilera <edgar.nicolas.aguilera@gmail.com>
2019-03-14 03:30:50 -0300

    Agregando estilos página listado de productos

--- client/src/components/ProductSummary/ProductSummary.component.js
+++ client/src/components/ProductSummary/ProductSummary.component.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './productSummary.style.css';
+
 const ProductSummary = ({ product }) => {
 	return (
-		<li>
-			<Link to={`/items/${product.id}`} title="Ver detalles del producto">
-				<img alt={product.title} src={product.picture} />
+		<li className="product-summary">
+			<Link className="product-image-container" to={`/items/${product.id}`} title="Ver detalles del producto">
+				<img className="product-image" alt={product.title} src={product.picture} />
 			</Link>
-			<div>{product.price.currency} {Number(product.price.amount).toLocaleString('es')}</div>
-			{product.free_shipping && <mark><i>Envio Gratuito</i></mark>}
-			<h2>{product.title}</h2>
-			<h5>{product.address}</h5>
+			<header className="product-summary-header">
+				<div className="product-summary-header__price">{product.price.currency} {Number(product.price.amount).toLocaleString('es')}</div>
+				{product.free_shipping && <i className="icon icon--shipping">Envio Gratuito</i>}
+				<h2 className="product-summary-header__title">{product.title}</h2>
+			</header>
+			<aside className="product-summary-address">
+				<h5 className="product-summary-address__location">{product.address}</h5>
+			</aside>
 		</li>
 	);
 }
--- client/src/components/ProductsList/ProductsList.component.js
+++ client/src/components/ProductsList/ProductsList.component.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import ProductSummary from '../ProductSummary/ProductSummary.component';
 
+import './productsList.style.css';
+
 const ProductList = ({ products = [] }) => {
 	return (
-		<section>
-			<ol>
+		<section className="products-list">
+			<ol className="product-list-container">
 				{products.map(product => <ProductSummary key={product.id} product={product} />)}
 			</ol>
 		</section>
